0fcb3dd105510eecc72973c524c662657c964f52
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-08-06 12:13:51 -0300

    setting sound alarms

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -3,6 +3,14 @@ import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
 import { Timer } from './timer';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../sounds/bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../sounds/bell-finish.mp3');
+
+const auidoStartWorking = new Audio(bellStart);
+const auidoStopWorking = new Audio(bellFinish);
+
 interface Props {
   pomodoroTime: number;
   shortRestTime: number;
@@ -33,6 +41,7 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true);
     setResting(false);
     setMainTime(props.pomodoroTime);
+    auidoStartWorking.play();
   };
 
   const configureRest = (long: boolean) => {
@@ -45,6 +54,8 @@ export function PomodoroTimer(props: Props): JSX.Element {
     } else {
       setMainTime(props.shortRestTime);
     }
+
+    auidoStopWorking.play();
   };
 
   return (
